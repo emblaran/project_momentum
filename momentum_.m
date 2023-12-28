5f941d017ee58b4e8e4e5b1010fc794ee1084844
emblaran <153724697+emblaran@users.noreply.github.com>
2023-12-28 11:49:46 +0800

    Update momentum_.m

--- momentum_.m
+++ momentum_.m
@@ -20,3 +20,15 @@ end
 % Displaying average returns for value-weighted portfolios
 disp('Average Value-Weighted Returns for Each Portfolio:');
 disp(average_returns);
+
+% Function to sort stocks into five groups based on previous K months' return
+function data_sorted = sort_stocks(data, K)
+    % Add code here to sort stocks into five groups based on previous K months' return
+    % For example, you can use quantiles or other criteria
+    % Store the group information in a new variable, e.g., 'group'
+    data.group = discretize(data.return_m_lagged, linspace(min(data.return_m_lagged), max(data.return_m_lagged), 6), 'IncludedEdge', 'right');
+    data.group = categorical(data.group);
+    data.Properties.VariableNames{end} = 'group';
+    
+    data_sorted = data;
+end
