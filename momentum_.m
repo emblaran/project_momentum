42aa99b0237ad1b653ab4d059fe22d64e3eb3a3f
emblaran <153724697+emblaran@users.noreply.github.com>
2024-01-02 22:40:26 +0800

    Update momentum_.m

--- momentum_.m
+++ momentum_.m
@@ -1,21 +1,18 @@
+%% Examine the momentum effect
+
 clear
 close all
 
-
 return_m_hor=readtable('return_monthly.xlsx','ReadVariableNames',true,'PreserveVariableNames',true,'Format','auto');
-
 
 return_m=stack(return_m_hor,3:width(return_m_hor),'NewDataVariableName','return_m',...
 'IndexVariableName','date');
-writetable(return_m,'myPatientData.xlsx','WriteRowNames',true) 
 return_m.date=char(return_m.date);
 return_m.datestr=datestr(return_m.date);
 return_m.date=datetime(return_m.datestr,'InputFormat','dd-MMM-yyyy','Locale','en_US');
 return_m.return_m=return_m.return_m/100;
 
-
 % Read the file with previous month market capitalizaiton 
-
 
 market_cap_lm_hor=readtable('me_lag.xlsx','ReadVariableNames',true,'PreserveVariableNames',true,'Format','auto');
 market_cap_lm=stack(market_cap_lm_hor,3:width(market_cap_lm_hor),'NewDataVariableName','lme',...
@@ -29,142 +26,149 @@ return_monthly=outerjoin(return_m,market_cap_lm,'Keys',{'date','code','name','da
 return_monthly=sortrows(return_monthly,{'code','date'},{'ascend','ascend'});
 
 index=~isnan(return_monthly.lme);
-return_monthly=return_monthly(index, : );
-
+return_monthly=return_monthly(index,1:end);
 
 save return_m.mat return_monthly;
 
-%%
-%better edit date as yymm for we are conducting monthly analysis
-return_monthly.yymm = year(return_monthly.date) * 12 + month(return_monthly.date);
+%%  examine momentum strategy by creating a dateset with previous stock returns information
 
-% (b) Every K months, sort stocks into five groups based on previous K months' return and hold this position for K months. 
-% What is the average equal-weighted return spread between high and low previous stock returns portfolios 
-% for K = 1; 3; 6; 12; 24. Do you find that momentum exists in Chinese stock markets?
 
-% First we create a new dataset with K = 1; 3; 6; 12; 24;, which denotes
-% the frequency month returns.
+load('return_m.mat');
 
-% Define the values of K
-K_values = [1, 3, 6, 12, 24];
+% num_obs is the number of observatiosn in the sample 
 
-%Looks like we have different opinions on this question. My opinion is, we
-%should be sorting the stocks on "previous K months return" which should be
-%a cumulative return not the return K months ago
+[G,jdate]=findgroups(return_monthly.date);
+num_obs=length(jdate);
 
-% Loop over each value of K
-for i = 1:length(K_values)
-    K = K_values(i);
-    table_height = size(return_monthly, 1);
+% jdate is another index for indexing the dataset 
 
-    [G, code] = findgroups(return_monthly.code);
-    k_month_return_handle = @(input) k_month_return(input, K);
-    result_cell = splitapply(k_month_return_handle, return_monthly.return_m, G);
-    % Create a new column that contains K months cummulative return every K
-    % month, and name it "lagK"
-    new_column = ['lag', num2str(K)];
-    return_monthly.(new_column) = vertcat(result_cell{:});
+return_monthly.jdate=G;
 
-    %Issue: this group index seems not used
-    % Sort stocks into five groups based on previous K months' return
-    %return_m_k = sort_stocks(return_m_k, K);
+%frequency=[1,3,6,12,24];
+frequency=[3];
 
-    % Displaying the results
-    %disp(['Dataset with previous ', num2str(K), ' months return and sorted stocks:']);
-    %disp(return_m_k);    
+mom_old=table();
+
+tic 
+
+for i=frequency
+        
+    % We need frequency(i) points before and after for this strategy
+    for j=[i: num_obs-i]
+        % pick up previous frequency(i) months returns and need an index for
+        % selection for relevant observation
+         temp_date=[floor(j/i)*i-i+1:floor(j/i)*i-i+i];
+         start_date=j+1;
+         % furthermore, this date is updated every i months
+   
+
+        % this returns the relevent index for picking up returns 
+        index_i=(return_monthly.jdate==temp_date);
+       %  creates a composite index where the sample condition is met 
+        index=logical(sum(index_i,2));
+        mom_sample=return_monthly(index,1:end);
+        
+
+        
+       % next calculate the previous months' cumulative return for later
+       % portfolio analysis 
+       [G,code]=findgroups(mom_sample.code);
+       pr_return=splitapply(@(x)sum(x),mom_sample.return_m,G);
+       pr_return_table=table(code,pr_return);
+       % merge it back to mom_smaple to enhance the vector of previous
+       % return
+       
+       index_r=(return_monthly.jdate==start_date);
+       mom_r=return_monthly(index_r,1:end);
+
+
+       mom_sample1=outerjoin(mom_r,pr_return_table,'Keys',{'code'},'MergeKeys',true,'Type','left');
+       
+       % keep only the last obervation for each firm for later analysis
+       %mom_sample2=mom_sample1(mom_sample1.jdate==j+1,:);  %
+       % next merge the sample back to the full dataset for each iteration 
+
+       %return_full=outerjoin(return_monthly,mom_sample2(:,{'code','pr_return','jdate'}),'Keys',{'code','jdate'},'MergeKeys',true,'Type','left');
+       
+       return_full=vertcat(mom_old, mom_sample1); 
+       
+       mom_old=return_full;
+        
+    end
+
 end
 
-% So return_m_k is now our new dataset, based on the task in b).
+toc
 
-%% Portfolio Analysis 
+%%
 
-% We can split portfolio analysis into the follwing steps:
-% Calculate the breakpoints that will be used to divide the sample into portfolios
-% Use these breakpoints to form the portfolios
-% Calculate the average value of the outcome variable Y with each portfolio for each period t
-% Examine variation in these average values of Y across the different portfolios
+% return_full is the new dataset with previous K=frequency months return 
+% Next is to use the usual portfolio analysis of dividing into five
+% portfolios 
 
-% We have already formed the portfolios so we only have to do the last two
-% steps.
+% Create percentiles functions
 
-% Number of portfolios
-N = 5; %for different values of K
-
-%This isn't quite right, e.g the numbers of bin==5 is 6 which is definitely
-%not properly devided
-% Sorting stocks into N portfolios based on lagged stock returns
-%[~, edges, bin] = histcounts(return_m_k.return_m_lagged, N);
-% We have already sorted stocks by return_m_k - must be specify to
-% return_m_k.lagged
-
-
-%loop for K
-for i=1:5
-    K = K_values(i);
-    clear return_k_sorted
-
-    %Extract needed rows
-    indices = ~isnan(return_monthly.("lag"+num2str(K)));
-    return_k = return_monthly(indices, ["code","name","yymm","lme","lag"+num2str(K)]);
-
-    %holding return, every stock is held for K months, so the holding
-    %return is simply the next value in "lagK"
-    return_k.holding_return = [return_k.("lag"+num2str(K))(2:end); 0];
-
-    %grouping the stocks into 5 groups by lagK on a certain time stamp
-    return_k_sorted = lagged_return_port(return_k, 5, "yymm", "lag"+num2str(K));
-
-    %group by yymm and portfolio
-    [G, yymm, ir_port] = findgroups(return_k_sorted.yymm, return_k_sorted.lr_port);
-    %equal weighted average return
-    equal_weighted_avgr = splitapply(@mean, return_k_sorted.holding_return, G);
-    ewavgr_table = table(yymm, ir_port, equal_weighted_avgr);
- 
-    ewavgr_unstack = unstack(ewavgr_table, "equal_weighted_avgr", "ir_port");
-    %delete nan rows
-    ewavgr_unstack = rmmissing(ewavgr_unstack, 1);
-    %holding returns
-    hold_return = @(input) cum_return(input, K);
-    holding_returns = varfun(hold_return, ewavgr_unstack(:, 2:end));
-    disp(holding_returns);
-    %highest previous stock return minus lowest
-    MOM_factor_k = ewavgr_unstack.x5 - ewavgr_unstack.x1;
-
-    %saving data
-    save(['ewavgr_unstack_',num2str(K),'months.mat'], "ewavgr_unstack");
-    save(['MOM_factor_',num2str(K),'months.mat'], 'MOM_factor_k');
+for i=20:20:80
+   eval(['prctile_',num2str(i),'=','@(input)prctile(input,i)',';']);
 end
 
+% Calcualte percentiles
 
-%I'm not sure but this paraphrase of codes seems unnecessary to me
-%Computing value-weighted returns for each portfolio
-%for i = 1:N
-%    portfolioIdx = (bin == i);
-%    return_m_k.portfolio_returns(portfolioIdx) = return_m_k.return_m(portfolioIdx) .* return_m_k.return_m_lagged(portfolioIdx); %.*return_m_k.me(portfolioIdx)
-%end
+for x=20:20:80
+                eval(['b','=','prctile_',num2str(x),'(return_full.pr_return)',';']);
+                eval(['return_full.mom',num2str(x),'=','b*ones(size(return_full,1),1)',';']);
+end
 
-%Bug: not "value-weighted", the project requires "equal-weighted"
+return_full.mom_label=rowfun(@mom_bucket_5,return_full(:,{'pr_return','mom20','mom40','mom60'...
+                ,'mom80'}),'OutputFormat','cell');
+            
+return_full.ew=ones(size(return_full,1),1);        
+            
+            
+[G,jdate,mom_label]=findgroups(return_full.date, return_full.mom_label);
 
-% Calculating average //value_weighted// equal-weighted returns for each portfolio
-%for i = 1:N
-%    portfolioIdx = (bin == i);
-%    average_returns(i) = nanmean(return_m_k.return_m(portfolioIdx));
-%end
+ewret=splitapply(@wavg,return_full(:,{'return_m','ew'}),G);
+
+ewret_table=table(jdate,mom_label,ewret);
+
+mom_factors=unstack(ewret_table(:,{'ewret','jdate','mom_label'}),'ewret','mom_label');
+
+A=nanmean(table2array(mom_factors(:,2)))*100;
+
+E=nanmean(table2array(mom_factors(:,6)))*100;
+
+fprintf('The average return for the low previous return group is %4.3f percent per month \n',A)
+
+fprintf('The average return for the high previous return group is %4.3f percent per month \n',E)
+
+%% PCA and MOM factor
+
+% Let us illusrate this for $K=3$
+
+mom_pca=table2array(mom_factors(:,2:6));
+
+[coefMatrix score latnt tsquared explainedVar]=pca(mom_pca); %array_data instead of mom_pca
+
+factors=mom_pca*coefMatrix(:,1:5);
+
+plot(coefMatrix(:,1:3),'-x');
+legend('First','Second','Third')
+
+
+mom=mom_pca(:,5)-mom_pca(:,1);
+
+corr(factors(:,1),mom)
+
+corr(factors(:,2),mom)
+
+corr(factors(:,3),mom)
+
+corr(factors(:,4),mom)
+
+corr(factors(:,5),mom)
 
 
 
-%%
-%(c)
-%load K=3 data
-load("ewavgr_unstack_3months.mat","ewavgr_unstack");
-load("MOM_factor_3months.mat", "MOM_factor_k");
-
-%pca analysis
-ewavgr_unstack = table2array(ewavgr_unstack(:, 2:end));
-[coeff, ~, ~, ~, explained, mu] = pca(ewavgr_unstack);
-factors = ewavgr_unstack*coeff;
-PC1_3 = factors(:, 1:3);
-%%
 
 
 
