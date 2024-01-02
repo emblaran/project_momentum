7c7c9b5c029190768406c610cdb0534fb58c9ad3
Liang-Yi-fan <128769906+Liang-Yi-fan@users.noreply.github.com>
2024-01-02 08:00:37 +0800

    slight renew

--- momentum_class.m
+++ momentum_class.m
@@ -6,6 +6,9 @@ close all
 
 return_m_hor=readtable('return_monthly.xlsx','ReadVariableNames',true,'PreserveVariableNames',true,'Format','auto');
 
+%stack to reshape the monthly return table
+return_m = stack(return_m_hor, 3:size(return_m_hor, 2), 'NewDataVariableName','return_m',...
+          'IndexVariableName','date');
 
 return_m.date=char(return_m.date);
 return_m.datestr=datestr(return_m.date);
@@ -14,9 +17,10 @@ return_m.return_m=return_m.return_m/100;
 
 
 % Read the file with previous month market capitalizaiton 
-
-
 market_cap_lm_hor=readtable('me_lag.xlsx','ReadVariableNames',true,'PreserveVariableNames',true,'Format','auto');
+%stack to reshape the previous month market capitalizaiton 
+market_cap_lm = stack(market_cap_lm_hor, 3:size(market_cap_lm_hor, 2),'NewDataVariableName','market_cap_lm',...
+          'IndexVariableName','date');
 
 market_cap_lm.date=char(market_cap_lm.date);
 market_cap_lm.datestr=datestr(market_cap_lm.date);
